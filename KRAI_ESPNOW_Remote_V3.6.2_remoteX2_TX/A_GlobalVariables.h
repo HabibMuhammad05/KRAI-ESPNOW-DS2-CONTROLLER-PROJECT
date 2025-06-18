a329d01098cb9ca15c86db553e8d0738e6efebb4
Habib Muhammad <zihnimiftah12@gmail.com>
2025-06-18 15:06:24 +0700

    updated code

---
+++ KRAI_ESPNOW_Remote_V3.6.2_remoteX2_TX/A_GlobalVariables.h
@@ -0,0 +1,79 @@
+#include <WiFi.h>
+#include <esp_now.h>
+
+#include <Arduino.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <AsyncElegantOTA.h>
+#include <EEPROM.h>
+
+#include <U8g2lib.h>
+
+bool settingMode;
+unsigned long periodMs;
+float frequencyHz = 0.0;
+
+//============================EDIT THIS TO CHANGE THE RX MAC ADDRESS==================================//
+//uint8_t broadcastAddress[] = {0xE8, 0x6B, 0xEA, 0xD4, 0xA5, 0x84}; //esp32 ext antenna
+//uint8_t broadcastAddress[] = {0x8C, 0x4F, 0x00, 0x3C, 0x91, 0x7C}; // esp32 v1 microUSB
+
+uint8_t broadcastAddress[4][6] = {
+  {0x80, 0x7D, 0x3A, 0xEA, 0xB1, 0x98}, // esp32 ext ant R1
+  {0x8C, 0x4F, 0x00, 0x3C, 0x91, 0x7C}, // esp32 v1 microUSB 
+  {0x14, 0x33, 0x5C, 0x02, 0x34, 0x58}, // esp32 ext antenna 2
+  {0x14, 0x33, 0x5C, 0x02, 0x49, 0x58}  // esp32 ext antenna 3
+};
+int macIndex;
+
+//=========================VARS TO store incoming data - MUST MATCH WITH TX================================//
+typedef struct receivedData {
+  int rpmData[2];
+  float voltData[3];
+  int pressureData;
+} receivedData;
+receivedData incomingData; //struct to store incoming data.
+
+//=========================VARS TO SENT BY ESPNOW - MUST MATCH WITH RX================================//
+typedef struct struct_message {
+  bool stat[15];
+  int joyData[4];
+  uint8_t remoteIndex;
+//  char address[18];
+} struct_message;
+struct_message sendData; //struct for sending data.
+
+//==============================VARS FOR DS2 CONTROLLER BUTTONS=======================================//
+//uint8_t button[15] = {33,   18,   23,   19,   4,     15,     13,     12,       25,      26,      27,        14,      16,   17,  2}; 
+uint8_t button[15] = {33,   18,   16,   17,   4,     15,     13,     12,       25,      26,      27,        14,      19,   23,  32}; 
+                  // "L3", "R3", "L2", "L1", "UP", "LEFT", "DOWN", "RIGHT", "SQUARE", "CROSS", "ROUND", "TRIANGLE", "R2", "R1", SW
+                  //   0    1     2     3     4       5       6       7         8         9       10        11       12    13   14
+
+uint8_t joy1XPin = 34; 
+uint8_t joy1YPin = 35; 
+uint8_t joy2XPin = 36; 
+uint8_t joy2YPin = 39; 
+
+const int NUM_SAMPLES = 3;
+static int joy1XBuffer[NUM_SAMPLES] = {0}, joy1YBuffer[NUM_SAMPLES] = {0};
+static int joy2XBuffer[NUM_SAMPLES] = {0}, joy2YBuffer[NUM_SAMPLES] = {0};
+static int joy1XSum = 0, joy1YSum = 0, joy2XSum = 0, joy2YSum = 0;
+static int joy1XIndex = 0, joy1YIndex = 0, joy2XIndex = 0, joy2YIndex = 0;
+
+int joy1XCenter, joy1YCenter, joy2XCenter, joy2YCenter;
+int joy1XPos, joy1YPos, joy2XPos, joy2YPos;
+
+const int boxSize = 30;
+const int joy1XBox = 48;
+const int joy1YBox = 47;
+const int joy2XBox = 80;
+const int joy2YBox = 47;
+
+const int centerThreshold = 530;
+
+//==============================VARS FOR BATT VOLTAGE MEASUREMENT=======================================//
+uint8_t voltPin = 32; 
+float batteryVoltage, batteryPercentage;
+const float voltageDividerFactor = 47.0 / (100.0 + 47.0);
+const float calibrationFactor = 1.12; 
+const float minVoltage = 6.0; 
+const float maxVoltage = 8.4; 
